4878c609860e5cd9ad01b3dba478638b4a98728d
scapy47 <scapy47>
2024-11-01 22:25:45 +0530

    Created 404 page , removed boilerplate and added tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Scapy47.github.io',
+  site: 'https://Scapy47.github.io',
+  integrations: [tailwind()],
 });
