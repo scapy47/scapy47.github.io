a59509f499a7ac9999c8b114121c32173d799375
scapy47 <scapy47>
2024-11-22 14:48:43 +0530

    vartical nav done added react for future

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Scapy47.github.io',
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
 });
