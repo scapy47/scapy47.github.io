240f9e65b40ded2a5dc271e332cecc8f4bdc64b4
scapy47 <scapy47>
2024-11-24 00:18:24 +0530

    added mdx working on dynamic paths for content

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Scapy47.github.io',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
 });
