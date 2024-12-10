e043d97865d63af513b7091c4e7fb15b2164353f
scapy47 <scapy47>
2024-12-10 21:14:55 +0530

    created a simple wrapper for p5

--- src/components/demos/Demo.tsx
+++ src/components/demos/Demo.tsx
@@ -1,34 +1,13 @@
 import type p5 from "p5";
 import { useEffect, useRef } from "react";
 
-type P5ComponentProps = {
-    [K in keyof p5]?: p5[K]
-    // [K in keyof p5]?: p5[K] extends (...args: any[]) => any
-    // ? (...args: Parameters<p5[K]>) => ReturnType<p5[K]>
-    // : never;
-};
+interface prop {
+    sketch: (p5: p5) => void;
+    className?: string,
+}
 
-
-export default function Demo(prop: P5ComponentProps) {
+export default function Demo({ sketch, className }: prop) {
     const canvasRef = useRef<HTMLDivElement>(null)
-
-    function sketch(p5: p5) {
-        console.log(p5);
-        
-        p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
-
-        p5.draw = () => {
-            p5.background(250);
-            p5.normalMaterial();
-            p5.push();
-            p5.rotateZ(p5.frameCount * 0.01);
-            p5.rotateX(p5.frameCount * 0.01);
-            p5.rotateY(p5.frameCount * 0.01);
-            p5.plane(100);
-            p5.pop();
-        };
-    }
-
 
     useEffect(() => {
         let canvasInstance: p5
@@ -51,6 +30,6 @@ export default function Demo(prop: P5ComponentProps) {
         }
     }, [])
 
-    return <div ref={canvasRef} />
+    return <div className={className} ref={canvasRef} />
 
 }
