7970c8eff5df117188641357c83ebe12af91f9ac
scapy47 <scapy47>
2025-01-04 21:53:31 +0530

    changed collection name and organized files

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,7 @@ import { z, defineCollection } from "astro:content";
 
 // Export a single `collections` object to register your collection(s)
 export const collections = {
-    physics: defineCollection({
+    notes: defineCollection({
         loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/contents" }),
     })
 };
