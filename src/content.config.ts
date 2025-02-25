d6dac91b877a9a32590ab26d70be08dc74ee2767
scapy_47 <134828740+Scapy47@users.noreply.github.com>
2025-02-25 16:32:27 +0530

    ....

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,6 +5,12 @@ import { z, defineCollection } from "astro:content";
 // Export a single `collections` object to register your collection(s)
 export const collections = {
     notes: defineCollection({
-        loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/contents" }),
+        loader: glob({
+            pattern: [
+                "**/[^_]*.{md,mdx}",
+                "*.{md,mdx}"
+            ],
+            base: "./src/contents"
+        }),
     })
 };
