b0bfe5710de96a2023fd0e2450d4d4f771e003cc
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-04-19 19:13:00 +0530

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clone your Neovim configuration
-git clone https://github.com/Scapy47/nvim-config.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim && nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+git clone https://github.com/Scapy47/nvim-config.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim && nvim --headless
 
 # Install common LSP servers (adjust based on your config's needs)
 npm install -g typescript-language-server # For TypeScript/JavaScript
@@ -14,12 +14,5 @@ deno --version
 python3 --version
 manim --version
 nvim --version
-
-# Set up a sample Manim project
-mkdir -p /home/nvimuser/workspace/src/manim
-echo "from manim import *\n\nclass SquareToCircle(Scene):\n    def construct(self):\n        circle = Circle()\n        square = Square()\n        self.play(Create(square))\n        self.play(Transform(square, circle))\n        self.wait()" > /home/nvimuser/workspace/src/manim/example.py
-
-# Ensure Neovim config permissions
-sudo chown -R nvimuser:nvimuser /home/nvimuser/.config/nvim
 
 echo "Neovim Dev Container setup complete!"
