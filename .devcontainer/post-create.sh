45b32835c80ddb524a8fe673d0ff400e88c65cc7
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-04-21 09:38:12 +0530

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,10 +3,6 @@
 # Clone your Neovim configuration
 git clone https://github.com/Scapy47/nvim-config.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim && nvim --headless
 
-# Install common LSP servers (adjust based on your config's needs)
-npm install -g typescript-language-server # For TypeScript/JavaScript
-pip3 install --user pyright # For Python
-# Deno LSP (denols) is built into Deno, already installed
 
 # Verify installations
 node --version
