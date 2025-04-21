18b4583b14932b97220f9641eb621054397d289f
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-04-21 16:41:09 +0530

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,7 +4,7 @@ sudo apt update && sudo apt upgrade
 
 sudo apt install -y 
          \ clang
-         \ neovim
+
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 sudo apt install build-essential python3-dev libcairo2-dev libpango1.0-dev texlive-full
@@ -12,13 +12,3 @@ manim --version
 
 curl -fsSL https://deno.land/install.sh | sh
 
-# ~Manually install NeoVim
-mkdir -p ~/.local
-cd ~/.local
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
-chmod u+x nvim-linux-x86_64.appimage
-./nvim-linux-x86_64.appimage --appimage-extract
-mkdir ~/.local/bin
-ln -s ~/.local/squashfs-root/AppRun ~/.local/bin/nvim
-## configuration
-git clone https://github.com/Scapy47/nvim.config.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
