96ffd6bd74e30dc79db52f828ec7bc927efc6a78
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-04-22 16:14:44 +0000

    ...

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y 
-         \ clang
-
-
-yes | curl -LsSf https://astral.sh/uv/install.sh | sh
-sudo apt install build-essential python3-dev libcairo2-dev libpango1.0-dev texlive-full
-manim --version
+	\ clang
 
 yes | curl -fsSL https://deno.land/install.sh | sh
-
