68cf2cf885fbd5b6c902332b1cd37b8f0b3c3468
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-04-22 22:11:44 +0530

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,23 +7,3 @@ sudo apt install -y
 
 yes | curl -fsSL https://deno.land/install.sh | sh
 
-echo "Setting up Homebrew directories..."
-mkdir -p /home/linuxbrew/.linuxbrew/bin
-ln -s /home/linuxbrew/.linuxbrew/Homebrew/bin/brew /home/linuxbrew/.linuxbrew/bin/brew
-
-echo "Adding Homebrew to shell environment..."
-{
-  echo '# Homebrew setup'
-  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
-} >> ~/.bashrc
-
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-echo "Updating Homebrew..."
-brew update --force --quiet
-
-echo "Securing permissions..."
-chmod -R go-w "$(brew --prefix)/share/zsh"
-
-echo "Homebrew installation complete!"
-brew --version
