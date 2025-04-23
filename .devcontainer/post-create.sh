7d86b0c9d21e650d05793ad358ea32a052c1c1b1
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-04-23 10:45:48 +0530

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
-
-sudo apt install -y 
-	\ clang
-
-NONINTERACTIVE=1 CI=1 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
-echo "# brew" >> ~/.bashrc
-echo >> ~/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-sudo brew install
-	\ neovim
-        \ deno
-	\ uv
 
 git clone https://github.com/Scapy47/nvim.config.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
