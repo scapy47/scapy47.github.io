7b0c1ad16b67cf4a86435830a1b5def0b24cd54a
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-04-23 10:12:07 +0530

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,5 +5,11 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y 
 	\ clang
 
-yes | curl -fsSL https://deno.land/install.sh | sh
+NONINTERACTIVE=1 CI=1 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 
+sudo brew install
+	\ neovim
+        \ deno
+	\ uv
+
+git clone https://github.com/Scapy47/nvim.config.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
