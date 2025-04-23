9ea40af129c1dc6b5c2f366febddaf31070e14bf
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-04-23 09:13:51 +0000

    ...

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,12 +9,12 @@ echo >> ~/.bashrc
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-sudo apt install -y 
-          \ clang
+sudo apt install -y \
+          clang
 
-sudo brew install
-          \ neovim
-          \ deno
-          \ uv
+brew install \
+	neovim \
+        deno \
+        uv 
 
 git clone https://github.com/Scapy47/nvim.config.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
