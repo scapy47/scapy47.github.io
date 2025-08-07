386dab798c9d53929e8e6a051dca991247b6e791
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-08-07 10:07:46 +0000

    ....

--- src/components/react/ui/Marquee.tsx
+++ src/components/react/ui/Marquee.tsx
@@ -1,11 +1,15 @@
 import type { ReactNode, ReactElement, CSSProperties } from "react"
 import { Children, cloneElement, isValidElement, useRef, useEffect } from "react";
-import style from "@/styles/snippet.module.css"
+import style from "./Marquee.module.css"
 
 type Props = {
   children: ReactNode,
   speed?: number
 }
+
+type Clone = ReactElement<{
+  className?: string, style: CSSProperties
+}>
 
 /**
  * @param {number} speed - controls the speed of marquee.
@@ -14,28 +18,23 @@ export default ({ children, speed = 10 }: Props) => {
 
   const rootRef = useRef<HTMLDivElement>(null)
   const relativeAnimationDuration = (speed / Children.count(children))
-  let uniqueDelay = 0
 
   useEffect(() => {
-    if (rootRef.current) {
-      rootRef.current.style.setProperty("--scrollSideDuration", `${speed}s`)
-    }
+    rootRef.current?.style.setProperty("--scrollSideDuration", `${speed}s`)
   }, [speed])
 
 
   return (
-    <div ref={rootRef} className="relative overflow-x-hidden overflow-y-visible flex">
+    <div ref={rootRef} className="relative overflow-x-hidden overflow-y-visible flex flex-nowrap">
       {
         Children.map(children, (child, index) => {
-
-          uniqueDelay = Children.count(children) - (index + 1)
-
+          const count = index + 1
           if (isValidElement(child) && typeof child.props === "object" && child.props !== null) {
-            return cloneElement(child as ReactElement<{ className?: string, style: CSSProperties }>, {
+            return cloneElement(child as Clone, {
               ...child.props,
               className: `${(child.props as any).className || ''} ${style.scrollSide}`.trim(),
               style: {
-                animationDelay: `${relativeAnimationDuration * uniqueDelay * -1}s`
+                animationDelay: `${relativeAnimationDuration * (Children.count(children) - count) * -1}s`
               }
             })
           }
