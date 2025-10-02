0910cdd2a21585ac8c9c4621acf261134159e9eb
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-10-02 16:42:02 +0000

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import remarkMath from "remark-math";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://Scapy47.github.io",
@@ -13,6 +15,10 @@ export default defineConfig({
       include: ["**/react/*"],
       experimentalReactChildren: true,
     }),
+    solidJs({
+      devtools: true,
+      include: ['**/solid/*', '**/node_modules/@suid/material/**'],
+    })
   ],
   markdown: {
     rehypePlugins: [rehypeKatex],
