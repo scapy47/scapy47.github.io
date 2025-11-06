8171f3a6ca86a11279aae7645b5572fdd7c0f384
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-11-06 09:23:54 +0000

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,26 @@
 import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Scapy47.github.io",
+
   integrations: [
     react({
       include: ["**/react/*"],
       experimentalReactChildren: true,
     }),
   ],
+
   markdown: {
     rehypePlugins: [rehypeKatex],
     remarkPlugins: [remarkMath],
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
