87980df8c6c32d3caa78e40df859724b2ce36f13
scapy47 <134828740+Scapy47@users.noreply.github.com>
2026-05-13 17:45:51 +0530

    moving from react to preact
    update
    
    ...
    
    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,25 @@
 import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
-import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://scapy47.pages.dev",
 
   integrations: [
-    react({
-      include: ["**/react/*"],
-      experimentalReactChildren: true,
-    }),
     icon({
       include: {
         "line-md": ["*"],
       }
+    }),
+    preact({
+      include: ["**/preact/*"],
+      compat: true,
     })
   ],
 
--- src/components/react/ui/window.tsx
+++
@@ -1,3 +0,0 @@
-export default function Window({}) {
-  return <div className={``}></div>;
-}
