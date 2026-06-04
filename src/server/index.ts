8fde59e572459c27bd2eab214642f19b022d55d9
scapy47 <134828740+Scapy47@users.noreply.github.com>
2026-06-04 23:16:23 +0530

    configed astro to reroute /ox to hono backend on dev, some style and other changes

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,16 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 
 export type AppType = {
   message: string
 }
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath('/xo')
+
+app.use(logger())
 
 app.get('/hello', (c) => {
   return c.json({ message: 'Hello from Hono!' })
 })
 
 export default app
-
