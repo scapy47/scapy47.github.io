3aba0fe463ff11a849457196c6693b139da148d0
scapy47 <134828740+Scapy47@users.noreply.github.com>
2026-07-10 16:28:32 +0530

    pin to vite@7
    ...

--- src/components/preact/ui/Carousel.tsx
+++ src/components/preact/ui/Carousel.tsx
@@ -2,13 +2,16 @@ import { cn } from "@/lib/utils";
 import type { ComponentProps } from "preact";
 
 type props = {
+  rep?: number
 } & ComponentProps<"div">;
 
-const Carousel = ({ children, className, ...rest }: props) => {
+const Carousel = ({ children, className, rep = 1, ...rest }: props) => {
   return (
     <div className={cn("flex w-full h-fit overflow-hidden", className)} {...rest}>
       <div className={"animate-carousel flex flex-none"}>{children}</div>
-      <div aria-hidden className={"animate-carousel flex flex-none"}>{children}</div>
+      {Array.from({ length: rep }, () => (
+        <div aria-hidden className={"animate-carousel flex flex-none"}>{children}</div>
+      ))}
     </div>
   )
 }
