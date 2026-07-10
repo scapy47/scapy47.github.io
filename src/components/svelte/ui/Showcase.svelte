784ce448fa3784b04647116d5c5b2c5b06a895ac
scapy47 <134828740+Scapy47@users.noreply.github.com>
2026-07-10 15:57:24 +0530

    new look
    ...

---
+++ src/components/svelte/ui/Showcase.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<section class="h-lvh overflow-scroll flex">
+  {#each Array.from({ length: 6 }) as _, index}
+    <div class="card min-w-2/6 max-w-4/6 min-h-2/6 max-h-4/6 ">
+      {index} card
+    </div>
+  {/each}
+</section>
