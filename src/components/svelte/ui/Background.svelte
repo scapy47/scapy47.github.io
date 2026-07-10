aaaf577de84684d8ec607464ae77fdc067f15137
scapy47 <134828740+Scapy47@users.noreply.github.com>
2026-07-10 15:57:24 +0530

    new look
    ...

--- src/components/svelte/ui/Background.svelte
+++ src/components/svelte/ui/Background.svelte
@@ -13,7 +13,7 @@
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 0;
 
-    renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+    renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true, });
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     const geometry = new THREE.SphereGeometry(1, 32, 32);
