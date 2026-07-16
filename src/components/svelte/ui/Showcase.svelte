5d9e1a322266d3ada1dc5059349ff22e123b3ed5
scapy47 <134828740+Scapy47@users.noreply.github.com>
2026-07-16 14:44:28 +0530

    fix symantics in html and small fixed

--- src/components/svelte/ui/Showcase.svelte
+++ src/components/svelte/ui/Showcase.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 </script>
 
-<section class="h-lvh overflow-scroll flex">
+<div class="flex overflow-scroll">
   {#each Array.from({ length: 6 }) as _, index}
-    <div class="card min-w-2/6 max-w-4/6 min-h-2/6 max-h-4/6 ">
+    <div class="card min-w-2/6 max-w-4/6 min-h-2/6 max-h-4/6">
       {index} card
     </div>
   {/each}
-</section>
+</div>
